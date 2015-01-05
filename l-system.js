e21e06bfe6299939cfa7b642f00b362a60357737
Emil Ng <github@alphabeticaldisorder.com>
2015-01-05 14:16:23 -0500

    blah

--- l-system.js
+++ l-system.js
@@ -3,24 +3,27 @@ var mk_rule = function (pred, succ) {
     return { pred: pred, succ: succ };
 };
 
+var rewrite = function (grammar, start, max_iter) {
+
+    var result = start;
+    var grammarLength = grammar.length;
+
+    for (var i = 0; i < max_iter; i++) {
+
+        // loop through grammar and replace
+        for (var j = 0; j < grammarLength; j++) {
+          result = result.replace(grammar.pred, grammar.succ);
+        }
+    }
+
+    return result;
+};
+
 var grammar = [
     mk_rule('A', 'AB'),
     mk_rule('B', 'A')
 ];
 
-var rewrite = function (start, max_iter) {
-
-    var result = start;
-
-    for (var i = 0; i < max_iter; i++) {
-
-        // loop through grammar and replace
-    }
-        
-    return result;
-};
-
-var result = rewrite('A', 10);
+var result = rewrite(grammar, 'A', 10);
 
 console.log(result);
-
