9871c519807551f2e372befeb6e897d9440e3e3e
Emil Ng <github@alphabeticaldisorder.com>
2015-10-28 17:55:42 -0400

    remove server.js because it's no longer used

--- server.js
+++
@@ -1,8 +0,0 @@
-var connect = require('connect');
-var http = require('http');
-// var path = require('path');
-
-var app = connect().use(connect.static('.'));
-
-http.createServer(app).listen(4000);
-
