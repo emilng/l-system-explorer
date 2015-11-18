9b6de2230247be31d672af8dced7c1a98d11b245
Emil Ng <github@alphabeticaldisorder.com>
2015-11-18 00:10:25 -0500

    refactor encodeInstructions into more testable code

--- src/data/encodeInstructions.js
+++ src/data/encodeInstructions.js
@@ -1,40 +1,49 @@
-var instructions = {
-  decode: function(instructionsString) {
-    var instructionsList = instructionsString.split(';');
-    return instructionsList.map(function (instructionParamString) {
-      var instructionParams = instructionParamString.split(',');
-      var instructions = {rule: window.unescape(instructionParams.shift())};
-      return instructionParams.reduce(function (params, paramString) {
-        var keyChar = paramString[0];
-        var keyLookup = {
-          'd': 'distance',
-          'a': 'angle',
-          'b': 'branch'
-        };
-        var key = keyLookup[keyChar];
-        var value = parseFloat(paramString.substr(1));
-        params[key] = value;
-        return params;
-      }, instructions);
-    });
-  },
-  encode: function(instructions) {
-    var encodedInstructions = instructions.map(function(instruction) {
-      var paramKeys = Object.keys(instruction);
-      paramKeys.splice(paramKeys.indexOf('rule'), 1);
-      var paramLookup = {
-          'distance': 'd',
-          'angle': 'a',
-          'branch': 'b'
-      };
-      var params = paramKeys.map(function(paramKey) {
-        return paramLookup[paramKey] + instruction[paramKey];
-      });
-      params.unshift(window.escape(instruction.rule));
-      return params.join(',');
-    });
-    return encodedInstructions.join(';');
-  }
+var decodeParams = function (params, paramString) {
+    var keyChar = paramString[0];
+    var keyLookup = {
+      'd': 'distance',
+      'a': 'angle',
+      'b': 'branch'
+    };
+    var key = keyLookup[keyChar];
+    var value = parseFloat(paramString.substr(1));
+    params[key] = value;
+    return params;
 };
 
-module.exports = instructions;
+var combineDecodedParams = function (paramString) {
+  var params = paramString.split(',');
+  var instructions = {rule: window.unescape(params.shift())};
+  return params.reduce(decodeParams, instructions);
+};
+
+var decode = function(instructionString) {
+  var instructionList = instructionString.split(';');
+  return instructionList.map(combineDecodedParams);
+};
+
+var encodeParams = function(instruction) {
+  var encodedRule = window.escape(instruction.rule);
+  delete instruction.rule;
+  var paramKeys = Object.keys(instruction);
+  var paramLookup = {
+      'distance': 'd',
+      'angle': 'a',
+      'branch': 'b'
+  };
+  var params = paramKeys.reduce(function(paramString, paramKey) {
+    return paramString + ',' + paramLookup[paramKey] + instruction[paramKey];
+  }, encodedRule);
+  return params;
+};
+
+var encode = function(instructions) {
+  var encodedInstructions = instructions.map(encodeParams);
+  return encodedInstructions.join(';');
+};
+
+exports.decodeParams = decodeParams;
+exports.combineDecodedParams = combineDecodedParams;
+exports.decode = decode;
+exports.encodeParams = encodeParams;
+exports.encode = encode;
--- tests/data/encodeInstructions.js
+++ tests/data/encodeInstructions.js
@@ -1,30 +1,61 @@
-var test = require('./encodeTestHelper.js');
+var test = require('tape');
 var encodeInstructions = require('../../src/data/encodeInstructions.js');
 
-test({
-  encoder: encodeInstructions,
-  name: 'encodeInstructions',
-  encoded: 'F,d4;+,a25;-,a-25;%5B,b0;%5D,b1',
-  decoded: [
-    {
-      rule: 'F',
-      distance: 4
-    },
-    {
-      rule: '+',
-      angle: 25
-    },
-    {
-      rule: '-',
-      angle: -25
-    },
-    {
-      rule: '[',
-      branch: 0
-    },
-    {
-      rule: ']',
-      branch: 1
-    }
-  ]
+var encoded = 'F,d4;+,a25;-,a-25;%5B,b0;%5D,b1';
+var decoded = [
+  {
+    rule: 'F',
+    distance: 4
+  },
+  {
+    rule: '+',
+    angle: 25
+  },
+  {
+    rule: '-',
+    angle: -25
+  },
+  {
+    rule: '[',
+    branch: 0
+  },
+  {
+    rule: ']',
+    branch: 1
+  }
+];
+
+test('encodeInstructions decodeParams', function(t) {
+  var actual = encodeInstructions.decodeParams({rule: 'F'}, 'd10,a20,b0');
+  var expected = {rule: 'F', distance: 10};
+  t.deepEqual(actual, expected);
+  t.end();
+});
+
+test('encodeInstructions combineDecodedParams', function(t) {
+  var actual = encodeInstructions.combineDecodedParams('F,d10,a20,b0');
+  var expected = {rule: 'F', distance: 10, angle: 20, branch: 0};
+  t.deepEqual(actual, expected);
+  t.end();
+});
+
+test('encodeInstructions decode', function(t) {
+  var actual = encodeInstructions.decode(encoded);
+  var expected = decoded;
+  t.deepEqual(actual, expected);
+  t.end();
+});
+
+test('encodeInstructions encodeParams', function(t) {
+  var actual = encodeInstructions.encodeParams({rule: 'F', distance: 10, angle: 20, branch: 0});
+  var expected = 'F,d10,a20,b0';
+  t.equal(actual, expected);
+  t.end();
+});
+
+test('encodeInstructions encode', function(t) {
+  var actual = encodeInstructions.encode(decoded);
+  var expected = encoded;
+  t.equal(actual, expected);
+  t.end();
 });
