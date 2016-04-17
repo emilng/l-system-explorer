01794c1319b367f35654d6bc545b2520e0c6852e
Emil Ng <github@alphabeticaldisorder.com>
2016-04-17 04:15:53 -0400

    rewrite l-systems.jsx in es2015 style

--- src/l-systems.jsx
+++ src/l-systems.jsx
@@ -1,68 +1,72 @@
-var React = require('react');
-var data = require('./data/data.js');
-var encodeHash = require('./data/encodeHash.js');
-var rewrite = require('./rewrite.js');
-var ui = require('./ui.js');
-var render = require('./render.js');
-var StartUI = require('./ui/start.jsx');
-var AxiomUI = require('./ui/axiom.jsx');
-var RuleContainerUI = require('./ui/ruleContainer.jsx');
-var InstructionContainerUI = require('./ui/instructionContainer.jsx');
+const React = require('react');
+const ReactDOM = require('react-dom');
+const encodeHash = require('./data/encodeHash.js');
+const rewrite = require('./rewrite.js');
+const ui = require('./ui.js');
+const render = require('./render.js');
+const StartUI = require('./ui/start.jsx');
+const AxiomUI = require('./ui/axiom.jsx');
+const RuleContainerUI = require('./ui/ruleContainer.jsx');
+const InstructionContainerUI = require('./ui/instructionContainer.jsx');
 
-var updateStep = 0;
-var UpdateEnum = {
+const UpdateEnum = {
   DECODE: 0,
   REWRITE: 1,
-  RENDER: 2
+  RENDER: 2,
 };
+const canvas = document.getElementById('canvas');
+const updateMethods = [decodeData, rewriteData, renderData];
 
-var setUpdateStep = function(stepNum) {
+let data = require('./data/data.js');
+let updateStep = 0;
+
+function setUpdateStep(stepNum) {
   if (stepNum < updateStep) {
     updateStep = stepNum;
   }
-};
+}
 
-var renderStartUI = function() {
-  React.render(
+function renderStartUI() {
+  ReactDOM.render(
     <StartUI data={data.start} update={renderStartUI} />,
     document.getElementById('start')
   );
   setUpdateStep(UpdateEnum.RENDER);
-};
+}
 
-var renderAxiomUI = function() {
-  React.render(
+function renderAxiomUI() {
+  ReactDOM.render(
     <AxiomUI data={data} update={renderAxiomUI} />,
     document.getElementById('axiom')
   );
-  setUpdateStep(UpdateEnum.PARSE);
-};
+  setUpdateStep(UpdateEnum.REWRITE);
+}
 
-var renderRulesUI = function() {
-  React.render(
+function renderRulesUI() {
+  ReactDOM.render(
     <RuleContainerUI data={data.rules} update={renderRulesUI} />,
     document.getElementById('rules')
   );
-  setUpdateStep(UpdateEnum.PARSE);
-};
+  setUpdateStep(UpdateEnum.REWRITE);
+}
 
-var renderInstructionsUI = function(reset) {
-  React.render(
+function renderInstructionsUI(reset) {
+  ReactDOM.render(
     <InstructionContainerUI reset={reset} data={data.instructions} update={renderInstructionsUI} />,
     document.getElementById('instructions')
   );
   setUpdateStep(UpdateEnum.RENDER);
-};
+}
 
-var canvas = document.getElementById('canvas');
-var exampleCallback = function() {
+
+function exampleCallback() {
   setUpdateStep(UpdateEnum.DECODE);
-};
+}
 ui.initExamples(data, exampleCallback);
 
-var decodeData = function() {
-  var hash = window.location.hash.substr(1);
-  //hardcoded default for now if nothing is set
+function decodeData() {
+  let hash = window.location.hash.substr(1);
+  // hardcoded default for now if nothing is set
   if (hash === '') {
     hash = 'F/F:F+F-F-F+F/F,d3.5;+,a75;-,a-80/6/x347,y358,a70,i6,z100';
   }
@@ -72,28 +76,26 @@ var decodeData = function() {
   renderRulesUI();
   renderInstructionsUI(true);
   ui.initCanvas(canvas, data, renderStartUI);
-};
+}
 
-var rewriteData = function() {
+function rewriteData() {
   data.rewrittenRules = rewrite.write(data.rules, data.axiom, data.iterations);
-};
+}
 
-var renderData = function() {
-  var iterations = Math.min(data.rewrittenRules.length - 1, data.start.iterations);
+function renderData() {
+  const iterations = Math.min(data.rewrittenRules.length - 1, data.start.iterations);
   render(canvas, data.start, data.rewrittenRules[iterations], data.instructions);
-  var urlHash = encodeHash.encode(data);
-  var stateObj = { data: urlHash };
-  history.replaceState(stateObj, 'L-Systems', 'index.html' + urlHash);
-};
+  const urlHash = encodeHash.encode(data);
+  const stateObj = { data: urlHash };
+  history.replaceState(stateObj, 'L-Systems', `index.html${urlHash}`);
+}
 
-var updateMethods = [decodeData, rewriteData, renderData];
-
-var update = function() {
-  while(updateStep < updateMethods.length) {
+function update() {
+  while (updateStep < updateMethods.length) {
     updateMethods[updateStep]();
     updateStep++;
   }
   requestAnimationFrame(update);
-};
+}
 
 requestAnimationFrame(update);
